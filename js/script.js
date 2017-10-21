7a4b1f2e0ff3e749854bdfd3d890538dbc4855e7
Curtis Beall <curtisbeall@outlook.com>
2017-10-20 20:29:28 -0700

    Started Shop Javascript
    Completed Filter Underline JS.
    Completed Type and Size Filter JS.

--- js/script.js
+++ js/script.js
@@ -4,29 +4,28 @@
 //Mouse Click Open
 $("#menu_btn").click(function (e) {
 	$("#menu").removeClass("hidden");
-    e.stopPropagation();
+	e.stopPropagation();
 });
 
 //Mouse Click Close
 $(document).click(function () {
-	if($("#menu").hasClass("hidden") === false) {
+	if ($("#menu").hasClass("hidden") === false) {
 		$("#menu").addClass("hidden");
 	}
 });
 
 //Enter Press Open
-$("#menu_btn").keyup(function(e){
-    if(e.keyCode === 13){
-        $("#menu_btn").click();
-    }
+$("#menu_btn").keyup(function (e) {
+	if (e.keyCode === 13) {
+		$("#menu_btn").click();
+	}
 });
 
-
 //Esc Press Close
-$(document).keyup(function(e){
-    if(e.keyCode === 27){
-        $(document).click();
-    }
+$(document).keyup(function (e) {
+	if (e.keyCode === 27) {
+		$(document).click();
+	}
 });
 
 
@@ -34,30 +33,84 @@ $(document).keyup(function(e){
 //////////////////////////////
 //Filter Toggle
 //////////////////////////////
-//Mouse Click Open
+//Mouse Click Toggle
 $("#filter_btn").click(function (e) {
-	$("#filter").removeClass("hidden");
-    e.stopPropagation();
-});
-
-//Mouse Click Close
-$(document).click(function () {
-	if($("#filter").hasClass("hidden") === false) {
+	if ($("#filter").hasClass("hidden") === false) {
 		$("#filter").addClass("hidden");
+		$("#filter_btn").removeClass("fill");
+	} else {
+		$("#filter").removeClass("hidden");
+		$("#filter_btn").addClass("fill");
+		e.stopPropagation();
 	}
 });
 
 //Enter Press Open
-$("#menu_btn").keyup(function(e){
-    if(e.keyCode === 13){
-        $("#menu_btn").click();
-    }
+$("#filter_btn").keyup(function (e) {
+	if (e.keyCode === 13) {
+		$("#filter_btn").click();
+	}
 });
-
 
 //Esc Press Close
-$(document).keyup(function(e){
-    if(e.keyCode === 27){
-        $(document).click();
-    }
+$(document).keyup(function (e) {
+	if (e.keyCode === 27 && $("#filter").removeClass("hidden")) {
+		mouseClickToggle();
+	}
 });
+
+///////////////////////////////
+//Filter Underline Length
+//////////////////////////////
+$("select").click(function () {
+	let textLength = $(this).val();
+	let removetext = document.getElementById('removetext');
+
+	removetext.innerHTML += "<h3 style='font-weight: bold;'><div id='remove'>" + textLength + "</div></h3>";
+	let width = $("#remove").width();
+	$("#remove").remove();
+	$(this).css("width", Math.ceil(width / 5) * 5 + 25 + "px");
+});
+
+$("#filter_btn").click(function () {
+	for (var i = 0; i < 9; i++) {
+		$("select").click()[i];
+	}
+});
+
+window.onload = (function () {
+	for (var i = 0; i < 9; i++) {
+		$("select").click()[i];
+	}
+});
+
+///////////////////////////////
+//Filter
+//////////////////////////////
+$("select").change(function () {
+			let showMe = $("#show_me").val();
+			let size = $("#size").val();
+
+			for (var i = 0; i < shoe.length; i++) {
+				let keep = false;
+				
+//				Check Shoe Type
+				if (shoe[i].type !== showMe && "ALL" !== showMe) {
+					let shoeName = shoe[i].name.toUpperCase()
+					$("a").find(":contains('" + shoeName + "')").hide();
+				} else {
+//					Check Shoe Size
+					for (var x = 0; x < shoe[i].size.length; x++) {
+						if (shoe[i].size[x] !== size && "ALL" !== size) {
+							let shoeName = shoe[i].name.toUpperCase()
+							$("a").find(":contains('" + shoeName + "')").hide();
+						} else {
+							keep = true;
+						}
+					}
+					if (keep === true) {
+						let shoeName = shoe[i].name.toUpperCase()
+						$("a").find(":contains('" + shoeName + "')").show();
+					}
+				}
+			});
---
+++ js/shoe_list.js
@@ -0,0 +1,47 @@
+///////////////////////////////
+//Shoe Information
+//////////////////////////////
+const shoe = [
+	//DONUT DREAMS
+		{
+			name: "DONUT DREAMS",
+			type: "SANDALS",
+			size: ["11", "10", "9"],
+			price: "$130.00"
+		},
+	//FREDDY KREUGER NMD
+		{
+			name: "FREDDY KREUGER",
+			type: "SNEAKERS",
+			size: ["11", "10"],
+			price: "$400.00"
+		},
+	//LUXURY FORCE 1 BLACK
+		{
+			name: "LUXURY FORCE 1 BLACK",
+			type: "SNEAKERS",
+			size: ["12", "11", "10", "9"],
+			price: "$400.00"
+		},	
+	//ADIDAS AKATSUKI SLIDE
+		{
+			name: "ADIDAS AKATSUKI SLIDE",
+			type: "SANDALS",
+			size: ["10", "9"],
+			price: "$150.00"
+		},		
+	//SUPER MARIO SB DUNK LOW
+		{
+			name: "SUPER MARIO SB DUNK LOW",
+			type: "SNEAKERS",
+			size: ["12", "11", "10"],
+			price: "315.00"
+		},	
+	//TRUSSSSST ME GUCCI SLIDE
+		{
+			name: "TRUSSSSST ME GUCCI SLIDE",
+			type: "SANDALS",
+			size: ["10", "9"],
+			price: "$550.00"
+		},	
+];
