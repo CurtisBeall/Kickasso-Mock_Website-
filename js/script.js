87a0ab157836c2e751a643f26991c537ad4874ee
Curtis Beall <curtisbeall@outlook.com>
2017-10-25 01:42:51 -0700

    Finished Shop JavaScript
    Completed all JS functions for the shop to work correctly.
    (This includes functions that interact with the Shoe Info pages, such as the prev and next shoe selection.)
    Also updated CSS as needed on each page.

--- js/script.js
+++ js/script.js
@@ -59,12 +59,15 @@ $(document).keyup(function (e) {
 	}
 });
 
+
+
 ///////////////////////////////
 //Filter Underline Length
 //////////////////////////////
 $("select").click(function () {
 	let textLength = $(this).val();
-	let removetext = document.getElementById('removetext');
+	let removetext = document.getElementById('filterResults');
+
 
 	removetext.innerHTML += "<h3 style='font-weight: bold;'><div id='remove'>" + textLength + "</div></h3>";
 	let width = $("#remove").width();
@@ -79,38 +82,168 @@ $("#filter_btn").click(function () {
 });
 
 window.onload = (function () {
-	for (var i = 0; i < 9; i++) {
-		$("select").click()[i];
+	let shop = "/C:/Users/Curtis/Documents/Git%20Repos/Kickasso-Mock_Website-/shop.html"
+
+	if (window.location.pathname === shop) {
+		for (var i = 0; i < 9; i++) {
+			$("select").click()[i];
+		}
+		sortBy();
+		newResultList();
 	}
 });
 
+
+
 ///////////////////////////////
-//Filter
+//Filter By
 //////////////////////////////
 $("select").change(function () {
-			let showMe = $("#show_me").val();
-			let size = $("#size").val();
+	let showMe = $("#show_me").val();
+	let size = $("#size").val();
 
-			for (var i = 0; i < shoe.length; i++) {
-				let keep = false;
-				
-//				Check Shoe Type
-				if (shoe[i].type !== showMe && "ALL" !== showMe) {
-					let shoeName = shoe[i].name.toUpperCase()
+	for (var i = 0; i < shoe.length; i++) {
+		let keep = false;
+		let shoeName = shoe[i].name.toUpperCase()
+
+		//Check Shoe Type
+		if (shoe[i].type !== showMe && "ALL" !== showMe) {
+			//Hide Shoe That Don't Match Size'
+			shoe[i].keep = "false";
+			$("a").find(":contains('" + shoeName + "')").hide();
+		} else {
+			//Check Shoe Size
+			for (var x = 0; x < shoe[i].size.length; x++) {
+				if (shoe[i].size[x] !== size && "ALL" !== size) {
+					//Hide Shoe That Don't Match Size'
+					shoe[i].keep = "false";
 					$("a").find(":contains('" + shoeName + "')").hide();
 				} else {
-//					Check Shoe Size
-					for (var x = 0; x < shoe[i].size.length; x++) {
-						if (shoe[i].size[x] !== size && "ALL" !== size) {
-							let shoeName = shoe[i].name.toUpperCase()
-							$("a").find(":contains('" + shoeName + "')").hide();
-						} else {
-							keep = true;
-						}
-					}
-					if (keep === true) {
-						let shoeName = shoe[i].name.toUpperCase()
-						$("a").find(":contains('" + shoeName + "')").show();
-					}
+					keep = true;
 				}
-			});
+			}
+			//Show Shoe
+			if (keep === true) {
+				$("a").find(":contains('" + shoeName + "')").show();
+				shoe[i].keep = "true";
+			}
+		}
+	}
+	sortBy();
+	newResultList();
+});
+
+
+
+///////////////////////////////
+//Sort By
+//////////////////////////////
+function sortBy() {
+	let list = document.getElementById("filterResults");
+	let switching = true;
+	let shouldSwitch = false;
+
+	while (switching) {
+		switching = false;
+		let product = list.getElementsByTagName("a");
+		for (i = 0; i < (product.length - 1); i++) {
+			shouldSwitch = false;
+			let price = product[i].getElementsByTagName("h4")[0].innerHTML;
+			let priceNext = product[i + 1].getElementsByTagName("h4")[0].innerHTML;
+
+			if ($("#sort").val() === "NAME") {
+				//Sort By Name
+				if (product[i].innerHTML.toLowerCase() > product[i + 1].innerHTML.toLowerCase()) {
+					shouldSwitch = true;
+					break;
+				}
+			} else {
+				//Sort By Price
+				if (price > priceNext) {
+					shouldSwitch = true;
+					break;
+				}
+			}
+		}
+		if (shouldSwitch) {
+			product[i].parentNode.insertBefore(product[i + 1], product[i]);
+			switching = true;
+		}
+	}
+};
+
+
+
+///////////////////////////////
+//New Shoe List
+//////////////////////////////
+let newShoeList = [];
+
+function newResultList() {
+	newShoeList = [];
+	let list = document.getElementById("filterResults");
+	let product = list.getElementsByTagName("a");
+	let y = 0;
+
+	for (i = 0; i < product.length; i++) {
+		let name = product[i].getElementsByTagName("h3")[0].innerHTML.toUpperCase();
+
+		for (x = 0; x < shoe.length; x++) {
+			let shoeName = shoe[x].name.toUpperCase();
+
+			if (name === shoeName && shoe[x].keep === "true") {
+				if (newShoeList.length === 0) {
+					y = 0;
+					newShoeList[y] = shoe[x];
+					y += 1;
+				} else {
+					newShoeList[y] = shoe[x];
+					y += 1;
+				}
+			}
+		}
+	}
+	localStorage.setItem("newShoeList", JSON.stringify(newShoeList));
+}
+
+
+
+
+
+///////////////////////////////
+//Next/Prev Product
+//////////////////////////////
+function changeShoe(a) {
+	let currentShoe = document.getElementById("shoeInfo").getElementsByTagName("h1")[0].innerHTML;
+	let newURL;
+	let action = a.innerHTML;
+	let newList = JSON.parse(localStorage.getItem("newShoeList"));
+	console.log(newList)
+
+	for (i = 0; i < newList.length; i++) {
+		if (currentShoe === newList[i].name) {
+
+			if (action === "Next Item") {
+				if (i === newList.length - 1) {
+					newURL = newList[0].url;
+					break;
+				} else {
+					i += 1;
+					newURL = newList[i].url;
+					break;
+				}
+			} else if (action === "Previous Item") {
+				if (i === 0) {
+					i = (newList.length - 1);
+					newURL = newList[i].url;
+					break;
+				} else {
+					i -= 1;
+					newURL = newList[i].url;
+					break;
+				}
+			}
+		}
+	}
+	location.replace(newURL);
+}
--- js/shoe_list.js
+++ js/shoe_list.js
@@ -7,41 +7,53 @@ const shoe = [
 			name: "DONUT DREAMS",
 			type: "SANDALS",
 			size: ["11", "10", "9"],
-			price: "$130.00"
+			price: "$130.00",
+			url: "file:///C:/Users/Curtis/Documents/Git%20Repos/Kickasso-Mock_Website-/shoe_info_donut_dreams.html",
+			keep: "true"
 		},
 	//FREDDY KREUGER NMD
 		{
 			name: "FREDDY KREUGER",
 			type: "SNEAKERS",
 			size: ["11", "10"],
-			price: "$400.00"
+			price: "$400.00",
+			url: "file:///C:/Users/Curtis/Documents/Git%20Repos/Kickasso-Mock_Website-/shoe_info_freddy_kreuger.html",
+			keep: "true"
 		},
 	//LUXURY FORCE 1 BLACK
 		{
 			name: "LUXURY FORCE 1 BLACK",
 			type: "SNEAKERS",
 			size: ["12", "11", "10", "9"],
-			price: "$400.00"
-		},	
+			price: "$400.00",
+			url: "file:///C:/Users/Curtis/Documents/Git%20Repos/Kickasso-Mock_Website-/shoe_info_luxury_force_1_black.html",
+			keep: "true"
+		},
 	//ADIDAS AKATSUKI SLIDE
 		{
 			name: "ADIDAS AKATSUKI SLIDE",
 			type: "SANDALS",
 			size: ["10", "9"],
-			price: "$150.00"
-		},		
+			price: "$150.00",
+			url: "file:///C:/Users/Curtis/Documents/Git%20Repos/Kickasso-Mock_Website-/shoe_info_adidas_akatsuki_slide.html",
+			keep: "true"
+		},
 	//SUPER MARIO SB DUNK LOW
 		{
 			name: "SUPER MARIO SB DUNK LOW",
 			type: "SNEAKERS",
 			size: ["12", "11", "10"],
-			price: "315.00"
-		},	
+			price: "315.00",
+			url: "file:///C:/Users/Curtis/Documents/Git%20Repos/Kickasso-Mock_Website-/shoe_info_super_mario_sb_dunk_low.html",
+			keep: "true"
+		},
 	//TRUSSSSST ME GUCCI SLIDE
 		{
 			name: "TRUSSSSST ME GUCCI SLIDE",
 			type: "SANDALS",
 			size: ["10", "9"],
-			price: "$550.00"
-		},	
+			price: "$550.00",
+			url: "file:///C:/Users/Curtis/Documents/Git%20Repos/Kickasso-Mock_Website-/shoe_info_trusssssst_me_nike_slide.html",
+			keep: "true"
+		}
 ];
